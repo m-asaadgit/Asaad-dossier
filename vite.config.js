30616377c2194645c7e86f2626afd42c1d59f3bf
m-asaadgit <asaaddangi20@gmail.com>
2024-08-12 17:15:46 +0530

    removed router

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio/',  // Adjust this if your repository name is different
+
 })
